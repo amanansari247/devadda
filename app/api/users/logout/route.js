85a6e4311ba87561dd960f6e262edeeede44ffdb
amanansari247 <amantech247@gmail.com>
2024-02-02 23:34:21 +0530

    fixing logout

--- app/api/users/logout/route.js
+++ app/api/users/logout/route.js
@@ -1,19 +1,19 @@
 import { NextResponse } from "next/server";
 
-export async function GET(){
+export async function GET() {
     try {
         const resp = NextResponse.json({
-            message:'Logout Successfully',
-            sucess: true
-        })
+            message: 'Logout Successfully',
+            success: true
+        });
 
-        resp.cookies.set("token","",{
-            httpOnly:true,
+        resp.cookies.set("token", "", {
+            httpOnly: true,
             expires: new Date(0)
-        })
+        });
+
         return resp;
-        
     } catch (error) {
-        return NextResponse.json({error:error.message}, {status:400})
+        return NextResponse.json({ error: error.message }, { status: 400 });
     }
 }
