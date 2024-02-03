54fdfead4b2f75c2d37aba89d64799d73e08a102
amanansari247 <amantech247@gmail.com>
2024-02-03 21:25:01 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,34 +1,34 @@
-import { NextResponse } from 'next/server'
-import  { NextRequest } from 'next/server'
+// import { NextResponse } from 'next/server'
+// import  { NextRequest } from 'next/server'
  
 
-export function middleware(request=NextRequest) {
-  const path = request.nextUrl.pathname;
-  const publicpath = path==='/login' || path ==='/signup'  || path === '/forgetpassword' || path === '/'|| path === '/resetpassword' || path==='/resetpassword'|| path==='/verifyemail'
-  const token = request.cookies.get('token')?.value||'';
-  if(publicpath && token){
-    return NextResponse.redirect(new URL('/profile',request.nextUrl))
-  }
-  if(!publicpath && !token){
-    return NextResponse.redirect(new URL('/login',request.nextUrl))
-  }
+// export function middleware(request=NextRequest) {
+//   const path = request.nextUrl.pathname;
+//   const publicpath = path==='/login' || path ==='/signup'  || path === '/forgetpassword' || path === '/'|| path === '/resetpassword' || path==='/resetpassword'|| path==='/verifyemail'
+//   const token = request.cookies.get('token')?.value||'';
+//   if(publicpath && token){
+//     return NextResponse.redirect(new URL('/profile',request.nextUrl))
+//   }
+//   if(!publicpath && !token){
+//     return NextResponse.redirect(new URL('/login',request.nextUrl))
+//   }
 
-}
+// }
  
 
-export const config = {
-  matcher: [
-    '/',
-    '/profile',
-    '/login',
-    '/signup',
-    '/profile/:path*',
-    '/verifyemail',
-    '/createprojectpost',
-    '/resetpassword',
-    '/checkemail',
-    '/posts/:path*',
-    '/home',
-    '/posts'
-  ],
-}
+// export const config = {
+//   matcher: [
+//     '/',
+//     '/profile',
+//     '/login',
+//     '/signup',
+//     '/profile/:path*',
+//     '/verifyemail',
+//     '/createprojectpost',
+//     '/resetpassword',
+//     '/checkemail',
+//     '/posts/:path*',
+//     '/home',
+//     '/posts'
+//   ],
+// }
