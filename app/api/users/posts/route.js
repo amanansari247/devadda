8200a378f01c2dc1b8c1d157d80ee7e064eb0ea5
amanansari247 <amantech247@gmail.com>
2024-02-03 15:03:42 +0530

    jk

--- app/api/users/posts/route.js
+++ app/api/users/posts/route.js
@@ -4,7 +4,7 @@ import { NextRequest , NextResponse } from "next/server";
 
 connect();
 
-export default async function GET(request = NextRequest){
+export  async function GET(request = NextRequest){
     try {
         // Fetch all projects from the database
         const projects = await Project.find();
